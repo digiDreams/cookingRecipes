c1cf0e246eacf6ce3c5ac5e9b67686a273173995
digiDreams <digital2dreamer@gmail.com>
2022-10-06 15:32:42 +0200

    Adding project for structuring data Models

---
+++ cookingRecipes/cookingRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookingRecipesApp.swift
+//  cookingRecipes
+//
+//  Created by Apprenant 16 on 06/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct cookingRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
